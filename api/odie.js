31c184a8c07706283bd8ca01830bb0e8c3a9aa70
alpereninyeri <sethmuzikatolyesi@gmail.com>
2025-10-21 21:17:14 +0300

    Fix API to use CommonJS module system

--- api/odie.js
+++ api/odie.js
@@ -1,5 +1,5 @@
 // N8N Webhook Handler for Odie AI Assistant
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
     // CORS headers
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
